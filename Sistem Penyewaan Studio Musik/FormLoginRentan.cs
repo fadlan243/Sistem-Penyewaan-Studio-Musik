cb09ea062f4a917f72600b096b5bb206c542cbc1
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-13 05:07:53 +0700

    add koneksi database

--- Sistem Penyewaan Studio Musik/FormLoginRentan.cs
+++ Sistem Penyewaan Studio Musik/FormLoginRentan.cs
@@ -13,6 +13,7 @@ namespace Sistem_Penyewaan_Studio_Musik
 {
     public partial class FormLoginRentan : Form
     {
+        private readonly string connString = "Data Source=FADLANNASRIZAL\\FADLAN;Initial Catalog=StudioMusik_DB;Integrated Security=True";
        
     }
 }
