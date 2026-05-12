191ffe87e787465055796eb4583e7bde24002d63
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-12 19:50:50 +0700

    menambahkan method btnRegister

--- Sistem Penyewaan Studio Musik/FormDashboard.cs
+++ Sistem Penyewaan Studio Musik/FormDashboard.cs
@@ -14,6 +14,17 @@ namespace Sistem_Penyewaan_Studio_Musik
     public partial class FormDashboard : Form
     {
         SqlConnection conn = new SqlConnection("Data Source=FADLANNASRIZAL\\FADLAN;Initial Catalog=StudioMusik_DB;Integrated Security=True");
-        
+        public FormDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            // Membuka form registrasi
+            FormRegister formRegister = new FormRegister();
+            formRegister.Show();
+            this.Hide(); // Menyembunyikan form dashboard
+        }
     }
 }
