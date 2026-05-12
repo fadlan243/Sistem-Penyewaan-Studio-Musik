1b16cc661fa949ab07125bacd5631ef781ac9436
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-13 05:08:10 +0700

    public formloginrentan

--- Sistem Penyewaan Studio Musik/FormLoginRentan.cs
+++ Sistem Penyewaan Studio Musik/FormLoginRentan.cs
@@ -14,6 +14,10 @@ namespace Sistem_Penyewaan_Studio_Musik
     public partial class FormLoginRentan : Form
     {
         private readonly string connString = "Data Source=FADLANNASRIZAL\\FADLAN;Initial Catalog=StudioMusik_DB;Integrated Security=True";
-       
+        public FormLoginRentan()
+        {
+            InitializeComponent();
+        }
+
     }
 }
