16c358f2bfc08ac205c20c2a82d8595d3012729c
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-13 05:09:39 +0700

    btnSqlInjectionDemo

--- Sistem Penyewaan Studio Musik/FormLoginRentan.cs
+++ Sistem Penyewaan Studio Musik/FormLoginRentan.cs
@@ -110,6 +110,24 @@ namespace Sistem_Penyewaan_Studio_Musik
             this.Close();
         }
 
-        
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblUsername_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPassword_Click(object sender, EventArgs e)
+        {
+         
+        }
     }
 }
