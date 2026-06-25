832b28b6058aa23e3ac2312055d1db83d21633f2
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-06-25 13:46:16 +0700

    connection string formreportviewer

--- Sistem Penyewaan Studio Musik/FormReportViewer.cs
+++ Sistem Penyewaan Studio Musik/FormReportViewer.cs
@@ -4,3 +4,13 @@ using System.Data.SqlClient;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Sistem_Penyewaan_Studio_Musik
+{
+    public partial class FormReportViewer : Form
+    {
+        private readonly string connString = ("Data Source=192.168.110.121,1433;Initial Catalog=StudioMusik_DB;User ID=sa;Password=Masamba24032006;");
+
+        
+    }
+}
